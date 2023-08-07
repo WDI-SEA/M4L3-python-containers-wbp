b6b467a9ff15c3fa88797a24691fa1af89a0c1c9
drejmin <djjamn@gmail.com>
2023-08-07 10:11:34 -0700

    modified:   python/src/main.py 	modified:   python/src/contacts.py

--- python/src/main.py
+++ python/src/main.py
@@ -1,29 +1,35 @@
 import contacts
 
 addressbook = [
-  { name: 'Bruce Wayne', phone: '555-123-4567', email: 'bruce@wayne.com' },
-  { name: 'Clark Kent', phone: '555-222-3333', email: 'clark@dailyplanet.com' },
-  { name: 'Diana Prince', phone: '555-444-5555', email: 'diana@amazon.com' }
+  { 'name': 'Bruce Wayne', 'phone': '555-123-4567', 'email': 'bruce@wayne.com' },
+  { 'name': 'Clark Kent', 'phone': '555-222-3333', 'email': 'clark@dailyplanet.com' },
+  { 'name': 'Diana Prince', 'phone': '555-444-5555', 'email': 'diana@amazon.com' }
 ];
 
 def menu():
-  print('[1] Display all contacts');
-  print('[2] Add a new contact');
-  print('[3] Delete a contact');
-  print('[4] Exit');
+  print('[1] Display all contacts')
+  print('[2] Add a new contact')
+  print('[3] Delete a contact')
+  print('[4] Exit')
 
 def main():
-  run = True;
+  run = True
   while run:
-    menu();
-    selection = int(input('Enter a selection: '));
+    menu()
+    selection = int(input('Enter a selection: '))
   
-    match selection:
-      case 1: contacts.showContacts(addressbook); break;
-      case 2: contacts.addContact(addressbook);    break;
-      case 3: contacts.deleteContact(addressbook); break;
-      case 4: run = False; break;
-      case _: print('That selection is not valid, please try again!');
+    switch = {
+      1: lambda addressbook: contacts.showContacts(addressbook); break,
+      2: lambda addressbook: contacts.addContact(addressbook);    break,
+      3: lambda addressbook: contacts.deleteContact(addressbook); break,
+      4: lambda addressbook: quit()
+    
+     
+    if selection in match:
+    match[selection]()
+      _: lambda addressbook: print('That selection is not valid, please try again!');
+    
+
 
   print('Goodbye')
       
