10ba8e038b2f9fc72853bb2bef34f40666937964
drejmin <djjamn@gmail.com>
2023-08-07 02:04:04 -0700

    modified:   python/src/main.py

--- python/src/main.py
+++ python/src/main.py
@@ -1,11 +1,23 @@
+# //https://github.com/heapwolf/prompt-sync
+promptSync = require('prompt-sync');
+prompt = promptSync({ sigint: true });
+
 import contacts
 
-addressbook = []
+addressbook = [
+  { name: 'Bruce Wayne', phone: '555-123-4567', email: 'bruce@wayne.com' },
+  { name: 'Clark Kent', phone: '555-222-3333', email: 'clark@dailyplanet.com' },
+  { name: 'Diana Prince', phone: '555-444-5555', email: 'diana@amazon.com' }
+];
 
 def menu():
-  pass
+  print('[1] Display all contacts');
+  print('[2] Add a new contact');
+  print('[3] Delete a contact');
+  print('[4] Exit');
 
 def main():
+  
   pass
 
 main()
