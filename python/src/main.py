20d6feae04ae4f53dd519aa7cb1996be5d7222f5
weston-bailey <wdbailey87@gmail.com>
2023-08-07 13:24:22 -0400

    finish review

--- python/src/main.py
+++ python/src/main.py
@@ -3,9 +3,46 @@ import contacts
 addressbook = []
 
 def menu():
-  pass
+  print('[1] Display all contacts')
+  print('[2] Add a new contact')
+  print('[3] Delete a contact')
+  print('[4] Exit')
 
 def main():
-  pass
+  run = True
+  # def quit(addressbook):
+  #   nonlocal run
+  #   run = False
+
+  while run:
+    selection = input("Enter a selection: ").strip()
+
+    match selection:
+      case "1":
+        contacts.show_contacts(addressbook)
+      case "2":
+        contacts.add_contact(addressbook)
+      case "3":
+        contacts.delete_contact(addressbook)
+      case "4":
+        run = False
+      case _:
+        print("\nThat selection is not valid, please try again!\n")
+
+
+    # switch = {
+    #   "1": lambda addressbook : contacts.show_contacts(addressbook),
+    #   "2": lambda addressbook : contacts.add_contact(addressbook),
+    #   "3": lambda addressbook : contacts.delete_contact(addressbook),
+    #   "4": quit
+    # }
+
+    # # defualt case
+    # if selection in switch:
+    #   switch[selection](addressbook)
+    # else:
+    #   print("\nThat selection is not valid, please try again!\n")
 
 main()
+
+# my_function = lambda x, y: x + y
