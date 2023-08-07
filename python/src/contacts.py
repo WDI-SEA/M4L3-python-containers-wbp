add362152967b0d2b727366ef76fb21a8586dc17
asp <amandasueputney@gmail.com>
2023-08-07 10:15:39 -0700

    updates contacts

--- python/src/contacts.py
+++ python/src/contacts.py
@@ -1,8 +1,40 @@
 def show_contacts(addressbook):
-  pass
+  print()
+  for contact in addressbook:
+    print(f"{contact['name']} {contact['email']}: {contact['phone']} ")
+    print()
 
 def add_contact(addressbook):
-  pass
+  name = input("enter name: ")
+  phone = input("enter phone: ")
+  email = input("enter email: ")
+
+  new_contact = {
+    'name': name,
+    'phone': phone,
+    'email': email
+  }
+  addressbook.append(new_contact)
+
+  print(f'\n{name} was added.\n')
 
 def delete_contact(addressbook):
-  pass
+  pattern = input('Enter a part of their name: ').strip()
+
+  # idx = None
+  # for i, contact in enumerate(addressbook):
+  #   if pattern in contact["name"]:
+  #         idx = i
+
+  idx = None
+  for i in range(len(addressbook)):
+    if pattern in addressbook[i]["name"] and addressbook[i]["name"].index(pattern) >= 0:
+        idx = i
+          
+    if idx == None:
+       print("\nContact not found!\n")
+       return
+    
+    deleted_name = addressbook.pop(idx)
+
+    print(f"\n{deleted_name['name']} was deleted.\n")
