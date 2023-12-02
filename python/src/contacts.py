7e65f3956537090aaa52fbdcff3a271a61e9a48e
carldamey <carldamey@gmail.com>
2023-12-02 00:18:05 -0800

    finishes main.py and the first 2 functions of contacts.py

--- python/src/contacts.py
+++ python/src/contacts.py
@@ -1,8 +1,23 @@
 def show_contacts(addressbook):
-  pass
+  print()
+  for contact in addressbook:
+    print(f"{contact['name']} ({contact['email']}): ${contact['phone']}")
+  print()
 
 def add_contact(addressbook):
-  pass
+  name = input("Enter name: ").strip()
+  phone = input("Enter phone: ").strip()
+  email = input("Enter email: ").strip()
+
+  new_contact = {
+    "name": name,
+    "phone": phone,
+    "email": email,
+  }
+
+  addressbook.append(new_contact)
+
+  print(f"\n{name} was added.\n")
 
 def delete_contact(addressbook):
   pass
