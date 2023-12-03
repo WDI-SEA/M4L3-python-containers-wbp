e744c145fa895da7bcdc2a6724c8f0d0573974e8
SelamB <selambeyene2@gmail.com>
2023-12-02 20:02:27 -0800

    finishing deliverable

--- python/src/contacts.py
+++ python/src/contacts.py
@@ -1,8 +1,40 @@
 def show_contacts(addressbook):
-  pass
+  for contact in addressbook:
+    print(f'{contact["name"]} ({contact["email"]}): {contact["phone"]}')
 
 def add_contact(addressbook):
-  pass
+  name  = input('Enter name: ').strip()
+  phone = input('Enter phone: ').strip()
+  email = input('Enter email: ').strip()
+  newContact = {
+    'name': name,
+    'phone': phone,
+    'email': email
+  };
+  addressbook.append(newContact)
+  print(f'{name} was added.')
 
 def delete_contact(addressbook):
-  pass
+  pattern = input('Enter a part of their name: ').strip().lower()
+  for contact in addressbook:
+    if pattern in contact['name'].lower():
+      addressbook.remove(contact)
+      print(f'{contact["name"]} was deleted.')
+    else:
+      print('Contact Not found!')
+
+
+  # #   if pattern in contact['name'].lower():
+  # #     addressbook.remove(contact)
+  # #     print(f'{contact["name"]} was deleted.')
+  #   if pattern not in contact['name'].lower():
+  #     print('Contact Not found!')
+  #   else:
+  #     addressbook.remove(contact)
+  #     print(f'{contact["name"]} was deleted.')
+
+
+
+
+
+
