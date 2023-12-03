969aba15440108e886a0c1b49b329bad04f436a4
SelamB <selambeyene2@gmail.com>
2023-12-02 20:34:21 -0800

    debugging contacts.py

--- python/src/contacts.py
+++ python/src/contacts.py
@@ -2,6 +2,7 @@ def show_contacts(addressbook):
   for contact in addressbook:
     print(f'{contact["name"]} ({contact["email"]}): {contact["phone"]}')
 
+
 def add_contact(addressbook):
   name  = input('Enter name: ').strip()
   phone = input('Enter phone: ').strip()
@@ -12,7 +13,22 @@ def add_contact(addressbook):
     'email': email
   };
   addressbook.append(newContact)
-  print(f'{name} was added.')
+  print(f'\n{name} was added.\n')
+
+
+def delete_contact(addressbook):
+  pattern = input('Enter a part of their name: ').strip().lower()
+  idx = None
+  for i in range(len(addressbook)):
+    if pattern in addressbook[i]['name'].lower():
+      idx = i
+  if idx is None:
+    print(f'\nContact Not found!\n')
+    return
+  deleted_name = addressbook[idx]['name']
+  del addressbook[idx]
+  print(f'\n{deleted_name} was deleted.\n')
+
 
 # def delete_contact(addressbook):
 #   pattern = input('Enter a part of their name: ').strip().lower()
@@ -22,23 +38,6 @@ def add_contact(addressbook):
 #       print(f'{contact["name"]} was deleted.')
 #     else:
 #       print('Contact Not found!')
-
-
-def delete_contact(addressbook):
-  pattern = input('Enter a part of their name: ').strip().lower()
-  idx = None
-  for i in range(len(addressbook)):
-    if pattern in addressbook[i]['name'].lower():
-      idx = i
-  
-  if idx is None:
-    print('Contact Not found!')
-    return
-  
-  deleted_name = addressbook[idx]['name']
-  del addressbook[idx]
-  print(f'{deleted_name} was deleted.')
-
 
 
 
