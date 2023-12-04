9a4a841b33ac2cf8ddfc6d2fc6c07c709aa56573
lvlivingston <lvlivingston@gmail.com>
2023-12-04 17:44:11 -0600

    completed homework

--- python/src/contacts.py
+++ python/src/contacts.py
@@ -1,8 +1,36 @@
 def show_contacts(addressbook):
-  pass
+  print()
+  for contact in addressbook:
+      print(f"{contact['name']} -- ({contact['email']}) -- {contact['phone']}")
+  print()
 
 def add_contact(addressbook):
-  pass
+  name = input('Enter first and last name: ').strip()
+  phone = input('Enter phone: ').strip()
+  email = input('Enter email: ').strip()
+
+  new_contact = {
+      'name': name,
+      'phone': phone,
+      'email': email
+  }
+  addressbook.append(new_contact)
+
+  print(f'{name} was added to the address book.')
 
 def delete_contact(addressbook):
-  pass
+  prompt = input('Enter part of the contact name: ').strip()
+
+  index = None
+  for i, contact in enumerate(addressbook):
+      if prompt in contact['name'].lower():
+          index = i
+
+  if index is None:
+      print('That contact was not found!')
+      return
+
+  deleted_name = addressbook[index]['name']
+  del addressbook[index]
+
+  print(f'{deleted_name} was deleted from the address book.')
--- python/src/main.py
+++ python/src/main.py
@@ -1,11 +1,45 @@
 import contacts
 
-addressbook = []
-
 def menu():
-  pass
+  print('[1] Display all contacts');
+  print('[2] Add a new contact');
+  print('[3] Delete a contact');
+  print('[4] Exit');
 
 def main():
-  pass
+  
+  addressbook = [
+    {
+      'name': 'Bruce Wayne',
+      'email': 'bruce@wayne.com', 
+      'phone': '555-123-4567'
+    }, {
+      'name': 'Clark Kent',
+      'email': 'clark@dailyplanet.com', 
+      'phone': '555-222-3333'
+    }, {
+      'name': 'Diana Prince',
+      'email': 'diana@amazon.com', 
+      'phone': '555-444-5555'
+      }
+  ]
+
+  run = True
+  
+  while run:
+    menu()
+    selection = int(input('Enter a selection: '))
+
+    if selection == 1:
+        contacts.show_contacts(addressbook)
+    elif selection == 2:
+        contacts.add_contact(addressbook)
+    elif selection == 3:
+        contacts.delete_contact(addressbook)
+    elif selection == 4:
+        run = False
+        print('\nGoodbye!\n')
+    else:
+        print('\nThat selection is not valid, please try again!\n')
 
 main()
