31aa35e9a12fd2b182e84913f61d96b6ed4fa9bc
mHaines331 <mhaines9219@gmail.com>
2023-12-04 16:47:14 -0800

    add contacts

--- python/src/contacts.py
+++ python/src/contacts.py
@@ -1,8 +1,22 @@
 def show_contacts(addressbook):
-  pass
+    pass
+
 
 def add_contact(addressbook):
-  pass
+    name = input("Enter name: ")
+    phone = input("Enter phone: ")
+    email = input("Email: ")
+
+    new_contact = {
+        "name": name.strip(),
+        "phone": phone.strip(),
+        "email": email.strip(),
+    }
+
+    addressbook.append(new_contact)
+
+    console.log(f"{new_contact[name]} was added")
+
 
 def delete_contact(addressbook):
-  pass
+    pass
