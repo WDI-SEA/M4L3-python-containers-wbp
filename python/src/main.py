08dc2a839b908b15c6976f19063638f894b118ba
kfac <kfacison1998@gmail.com>
2023-12-05 21:18:07 -0600

    menu and main work but do not call contact

--- python/src/main.py
+++ python/src/main.py
@@ -1,11 +1,40 @@
 import contacts
 
-addressbook = []
+addressbook = [
+  { 'name': 'Bruce Wayne', 'phone': '555-123-4567', 'email': 'bruce@wayne.com' },
+  { 'name': 'Clark Kent', 'phone': '555-222-3333', 'email': 'clark@dailyplanet.com' },
+  { 'name': 'Diana Prince', 'phone': '555-444-5555', 'email': 'diana@amazon.com' }
+]
 
 def menu():
-  pass
+  print('[1] Display all contacts')
+  print('[2] Add a new contact')
+  print('[3] Delete a contact')
+  print('[4] Exit')
 
 def main():
-  pass
+  run = True
+  while run:
+    menu()
+    selection = int(input('Enter a selection: '))
+    if selection == 1:
+      print('\nYou have selected option 1\n')
+      #contacts.showContacts(addressbook)
+      # break
+    elif selection == 2:
+      print('\nYou have selected option 2\n')
+      #contacts.addContact(addressbook)
+      # break
+    elif selection == 3:
+      print('\nYou have selected option 3\n')
+      #contacts.deleteContact(addressbook)
+      # break
+    elif selection == 4:
+      run = False
+      # break
+    else:
+      print('\nThat selection is not valid, please try again!\n')
+
+  print('\nGoodbye!\n')
 
 main()
