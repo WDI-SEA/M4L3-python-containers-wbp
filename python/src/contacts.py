a3149a33d992f5d60e2da93d4a16e81572c336ef
mHaines331 <mhaines9219@gmail.com>
2023-12-07 17:20:06 -0800

    wbp pass

--- python/src/contacts.py
+++ python/src/contacts.py
@@ -1,8 +1,38 @@
 def show_contacts(addressbook):
-    pass
+    print(None)
+    for contact in addressbook:
+        print(f"{contact['name']} ({contact['email']}): {contact['phone']}")
+    print(None)
 
 
-g
+def add_contact(addressbook):
+    name = input("Enter name: ")
+    phone = input("Enter phone: ")
+    email = input("Email: ")
+
+    new_contact = {
+        "name": name.strip(),
+        "phone": phone.strip(),
+        "email": email.strip(),
+    }
+
+    addressbook.append(new_contact)
+
+    print(f"\n{new_contact['name']} was added.\n")
+
 
 def delete_contact(addressbook):
-    pass
+    pattern = input("Enter a part of their name: ").strip()
+    idx = None
+    for i, contact in enumerate(addressbook):
+        if contact["name"].find(pattern) != -1:
+            idx = i
+            break
+
+    if idx is None:
+        print("\nContact Not found!\n")
+        return
+    deleted_name = addressbook[idx]["name"]
+    del addressbook[idx]
+
+    print(f"\n{deleted_name} was deleted.\n")
