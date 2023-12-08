43e8c69e82f9216df4358eeb74c290ec86a87e20
Abay <ibrayev.abay@gmail.com>
2023-12-08 10:46:04 -0700

    so far so good

--- python/src/contacts.py
+++ python/src/contacts.py
@@ -1,8 +1,25 @@
 def show_contacts(addressbook):
-  pass
+  print('Showing all contacts: ')
+  for contact in addressbook:
+    print(f"{contact['name']} ({contact['email']}): {contact['phone']}")
+  print()
 
 def add_contact(addressbook):
-  pass
+  name = input('Enter name: ').strip()
+  phone = input('Enter phone: ').strip()
+  email = input('Enter email: ').strip()
+
+  new_contact = {
+    'name': name,
+    'phone': phone,
+    'email': email
+  }
+
+  addressbook.append(new_contact)
+  print(f'\n{name} was added.\n')
 
 def delete_contact(addressbook):
-  pass
+  pattern = input('Enter a part of their name: ').strip()
+  idx = None
+  for address in range(len(addressbook)):
+    print('ay')
